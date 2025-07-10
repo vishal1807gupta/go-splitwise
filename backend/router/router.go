5e3580588f1c0cb203c102b1407e8388ba05b6e5
Vishal Gupta <vishal1807gupta@gmail.com>
2025-07-10 10:40:09 +0530

    Update router.go

--- backend/router/router.go
+++ backend/router/router.go
@@ -31,6 +31,7 @@ func Router() *mux.Router {
 	r.HandleFunc("/api/getTransactions/{groupId}", controller.GetTransactions).Methods("GET")
 	r.HandleFunc("/api/insertTransactions/{groupId}", controller.InsertTransactions).Methods("POST")
 	r.HandleFunc("/api/trigger-monthly-reminders", controller.TriggerMonthlyReminders).Methods("POST")
+	r.HandleFunc("/api/wakeup", controller.WakeUp).Methods("POST")
 
 	return r
 }
